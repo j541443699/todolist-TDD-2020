2daf4705a56e3a9dc748fb26af3d3bfcc402ceb7
jyq <541443699@qq.com>
2020-04-14 10:22:20 +0800

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -28,6 +28,7 @@ class NewVisitorTest(unittest.TestCase):  # (1)
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
+        
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
@@ -37,12 +38,13 @@ class NewVisitorTest(unittest.TestCase):  # (1)
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list)table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows
                 )
         )
+        
         # There is still a text box inviting her to add another item. She
         # enters "Us peacock feathers to make a fly"(Edith is very methodical)
         self.fail('Finish the test!')
