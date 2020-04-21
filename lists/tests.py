26e1338b66a831bd8c16abc26b0dc8b73ffd1ba4
jyq <541443699@qq.com>
2020-04-21 11:15:01 +0800

    Be ready for list table refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -1,26 +1,15 @@
-# from django.urls import resolve
 from django.test import TestCase
-# from django.http import HttpRequest
 
-# from lists.views import home_page 
-
-class HomePageTest(TestCase):
-
-    #def test_root_url_resolve_to_home_page_view(self):
-    #    found = resolve('/') 
-    #    self.assertEqual(found.func,home_page) 
-
-    def test_home_page_returns_correct_html(self):
-        #request = HttpRequest() # 1
-        #response = home_page(request) # 2
-        response = self.client.get('/')
-        #html = response.content.decode('utf8') # 3
-        #self.assertTrue(html.startswith('<html>')) # 4
-        #self.assertIn('<title>To-Do lists</title>', html) # 5
-        #self.assertTrue(html.endswith('</html>')) # 4
-        
-
-        self.assertTemplateUsed(response, 'home.html') 
 # Create your tests here.
+class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html') 
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/',data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
 
 
