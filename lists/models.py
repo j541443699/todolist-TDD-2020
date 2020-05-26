80dc74aacebe5b672ccf7aa59ce8fa33f5b14067
jyq <541443699@qq.com>
2020-05-26 13:32:47 +0800

    Modify the database.

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+class List(models.Model): # models.Model包含save()
+    pass
 
 class Item(models.Model):
     text = models.TextField(default='')
+    # list = models.TextField(default='') # 'List object (1)' != <List: List object (1)>
+    list = models.ForeignKey(List,on_delete=models.CASCADE,default=None)
+
+
