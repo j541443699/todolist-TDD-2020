8f02a28093e81aa350db33a3c814a10a2743ab43
jyq <541443699@qq.com>
2020-06-02 15:15:59 +0800

    Use Bootstrap to imporve layout and set STATIC_ROOT in settings and disable admin

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -7,32 +7,7 @@ MAX_WAIT = 10
 import time
 
 
-class NewVisitorTest(LiveServerTestCase):
-
-    # def test_layout_and_styling(self):
-    #     # Edith goes to the home page
-    #     self.browser.get(self.live_server_url)
-    #     self.browser.set_window_size(1024, 768)
-
-    #     # She notices the input box is nicely centered
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     self.assertAlmostEqual(
-    #         inputbox.location['x'] + inputbox.size['width'] / 2,
-    #         512,
-    #         delta=10
-    #     )
-
-    #     # She starts a new list and sees the input is nicely centered there too
-    #     inputbox.send_keys('testing')
-    #     inputbox.send_keys(Keys.ENTER)
-    #     self.wait_for_row_in_list_table('1: testing')
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     self.assertAlmostEqual(
-    #         inputbox.location['x'] + inputbox.size['width'] / 2,
-    #         512,
-    #         delta=10
-    #     )
-
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):  # 在所有test之前执行
         self.browser = webdriver.Firefox() # 浏览器
@@ -149,3 +124,18 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
         
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
+        # She starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
